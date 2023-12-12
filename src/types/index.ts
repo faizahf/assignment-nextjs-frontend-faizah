a8e2ce155ef342dbc5686716f6ebef5ced7e6a37
faizahf <faizahs0212@gmail.com>
2023-12-12 14:36:11 +0700

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,49 @@
+export type User = {
+    id:         number;
+    name:       string;
+    email:      string;
+    password:   string;
+    role:       string;
+    membership: string;
+    balance:    number;
+    image:      string;
+    bookmarks:  Event[];
+    events:     Event[];
+}
+
+export type Event = {
+    id:          number;
+    name:        string;
+    startTime:   string;
+    duration:    string;
+    category:    string;
+    price:       number;
+    image:       string;
+    description: string;
+    capacity:    Capacity;
+}
+
+export type Capacity = {
+    max:       number;
+    remaining: number;
+}
+
+export type Merchandise = {
+    id:          number;
+    stock:       number;
+    price:       number;
+    image:       string;
+    name:        string;
+    description: string;
+    event:       Event[];
+}
+
+export type Purchase = {
+    id:            number;
+    userID:        number;
+    eventDate:     string;
+    purchaseDate:  string;
+    merchandises:  Merchandise[];
+    paymentStatus: boolean;
+    paymentTotal:  number;
+}
