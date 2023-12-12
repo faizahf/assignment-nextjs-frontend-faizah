99c072bdb3440ba0f84b1ca7b9f39786e96a5a0c
faizahf <faizahs0212@gmail.com>
2023-12-12 14:12:04 +0700

    feat: add useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+interface UseFetchResponse<T> {
+    data: T | null;
+    loading: boolean;
+    error: string | unknown;
+    fetchData: (
+        endpoint: string,
+        options: RequestInit | undefined
+    ) => Promise<void>;
+}
+
+const useFetch = <T,>(): UseFetchResponse<T> => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null | unknown>(null);
+    const url = 'http://localhost:9000'
+
+    const fetchData = async (
+        endpoint: string,
+        options: RequestInit | undefined
+      ) => {
+        try {
+          const response = await fetch(`${url}/${endpoint}`, options);
+    
+          if (!response.ok) {
+            throw new Error(
+              `Failed to feth data: ${response.status} ${response.statusText}`
+            ).message;
+          }
+    
+          const result = await response.json();
+          setData(result);
+        } catch (error: unknown) {
+          setError(error || "An error occured while fetching data");
+        } finally {
+          setLoading(false);
+        }
+    };
+
+    return {data, loading, error, fetchData};
+};
+
+export default useFetch;
