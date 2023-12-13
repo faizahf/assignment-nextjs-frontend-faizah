1b42eb4ec3aaa06b6900cca3145fe8ece5abfabb
faizahf <faizahs0212@gmail.com>
2023-12-13 11:53:52 +0700

    feat: add admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from '@/components/Sidebar/Sidebar'
+import React from 'react'
+
+function AdminHomePage() {
+  return (
+    <>
+        <Sidebar />
+    </>
+  )
+}
+
+export default AdminHomePage
