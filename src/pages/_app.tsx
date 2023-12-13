e85e266d72b5748fa3af51c7fa841e28f5945b53
faizahf <faizahs0212@gmail.com>
2023-12-13 11:53:29 +0700

    init daisy ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Source_Sans_3 } from "next/font/google";
+
+const source_sans = Source_Sans_3({
+    weight: ["400", "500", "600"],
+    subsets: ["latin"],
+  });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={source_sans.className} {...pageProps} />
 }
