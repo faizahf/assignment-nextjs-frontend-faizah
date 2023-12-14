0dea81d26fb2f594133a377212785b56221c9dab
faizahf <faizahs0212@gmail.com>
2023-12-14 10:23:26 +0700

    feat: update db

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,53 +1,54 @@
 export type User = {
-    id:         number;
-    name:       string;
-    email:      string;
-    password:   string;
-    role:       string;
-    membership: number;
-    balance:    number;
-    image:      string;
-}
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  membership: number;
+  balance: number;
+  image: string;
+};
 
 export type Event = {
-    id:          number;
-    name:        string;
-    date:   string;
-    time:   string;
-    price:       number;
-    image:       string;
-    description: string;
-    capacity:    Capacity;
-}
+  id: number;
+  name: string;
+  date: string;
+  time: string;
+  price: number;
+  image: string;
+  description: string;
+  location: string;
+  capacity: Capacity;
+};
 
 export type Capacity = {
-    total:       number;
-    booked: number;
-}
+  total: number;
+  booked: number;
+};
 
 export type Merchandise = {
-    id:          number;
-    stock:       number;
-    price:       number;
-    image:       string;
-    name:        string;
-    description: string;
-    eventId:     number;
-}
+  id: number;
+  stock: number;
+  price: number;
+  image: string;
+  name: string;
+  description: string;
+  eventId: number;
+};
 
 export type Purchase = {
-    id:            number;
-    userId:        number;
-    eventId:     number;
-    purchaseDate:  string;
-    merchandises:  Merchandise[];
-    isPaid: boolean;
-    paymentTotal:  number;
-}
+  id: number;
+  userId: number;
+  eventId: number;
+  purchaseDate: string;
+  merchandises: Merchandise[];
+  isPaid: boolean;
+  paymentTotal: number;
+};
 
 export type Bookmark = {
-    id: number;
-    userId: number;
-    eventId: number;
-    event?: Event;
-}
+  id: number;
+  userId: number;
+  eventId: number;
+  event?: Event;
+};
