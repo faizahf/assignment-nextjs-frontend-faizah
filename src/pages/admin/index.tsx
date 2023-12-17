cf3f928ed06d34e7332418ea652f827baec944c8
faizahf <faizahs0212@gmail.com>
2023-12-18 01:02:56 +0700

    feat: init dashboard page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,12 +1,14 @@
-import { Sidebar } from '@/components/Sidebar/Sidebar'
-import React from 'react'
+import { Sidebar } from "@/components/Sidebar/Sidebar";
+import React from "react";
 
 function AdminHomePage() {
   return (
     <>
-        <Sidebar />
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-semibold w-full">Dashboard</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default AdminHomePage
+export default AdminHomePage;
