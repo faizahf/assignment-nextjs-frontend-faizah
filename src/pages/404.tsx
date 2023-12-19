fb6a13ff0b8f14958142fee2d88467b5003e9909
faizahf <faizahs0212@gmail.com>
2023-12-19 21:35:14 +0700

    feat: fix image in not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button/Button'
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react'
 
@@ -6,7 +7,7 @@ function NotFoundPage() {
     const router = useRouter();
   return (
     <div className='m-auto text-center'>
-        <img src='img/not-found.png' />
+        <Image src={'/img/not-found.png'} alt={'not-found'} width={600} height={600}/>
         <p className='font-bold text-[36px] mt-10'>Oops!</p>
         <p className='text-grey text-2xl'>We can’t seem to find the page you are looking for</p>
         <Button styles={'btn-primary bg-primary rounded-full mt-10'} value={'Back to Homepage'} funcOnClick={() => router.push('/')} />
